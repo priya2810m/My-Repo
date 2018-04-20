958742b7228b2f6af85397ce84c8f14d26e93279
priya2810m <38342345+priya2810m@users.noreply.github.com>
2018-04-20 15:31:10 +0530

    committed on -20-Apr-2018
    new page

---
+++ src/app/employee.ts
@@ -0,0 +1,18 @@
+export class EmployeeModel{
+    aceNo;
+    userName;
+    jan;
+    feb;
+    mar;
+    apr;
+    may;
+    jun;
+    jul;
+    aug;
+    sep;
+    oct;
+    nov;
+    dec;
+    total;
+
+}
